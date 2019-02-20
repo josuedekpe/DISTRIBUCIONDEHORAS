87ff26a03dc5b094d84c7440a11e06bf3ae4eed4
Usuario <Usuario@User>
2019-02-20 17:45:55 -0500

    elaborando la pagina principal home

--- src/java/Conexion/conexion.java
+++ src/java/Conexion/conexion.java
@@ -21,7 +21,7 @@ public class conexion {
     public static String database = "CDHQUISPICANCHI";
     public static String url = "jdbc:mysql://" + host + "/" + database;
     public static String user = "root";
-    public static String pass = "";
+    public static String pass = "root";
     public static String Driver_MySql = "com.mysql.jdbc.Driver";
     //public static String Driver_SqlServer = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     //public static String Driver_Oracle = "oracle.jdbc.driver.OracleDriver";    
